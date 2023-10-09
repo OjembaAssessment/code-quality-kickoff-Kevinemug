9a9b9be0b436960e16689bb496f9e64e4c44675b
Mugisha Kevine <Kevinemug>
2023-10-09 15:39:30 +0200

    improvement 1 all test passed

--- src/improvement-1.js
+++ src/improvement-1.js
@@ -6,11 +6,9 @@
 
 // FUNCTION 1
 export default function cntRbt(ls) {
-  var cnt = 0;
-  for (var i = 0; i < ls.length; i++) {
-    if (ls[i] == "R") {
-      cnt++;
-    }
-  }
-  return cnt;
+  if (!ls || typeof ls !== 'string') return 0;
+  return ls.split('R').length - 1;
 }
+
+
+
