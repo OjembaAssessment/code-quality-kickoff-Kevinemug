138c16d8591f215dd24f0e28d91e69badf9b7211
Mugisha Kevine <Kevinemug>
2023-10-09 14:25:47 +0200

    password validator all test passed

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -1,26 +1,26 @@
 export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 
-/**
- * Checks if a given password is valid or invalid.
- * If valid it returns true, otherwise false
- * @param {string} password
- * @returns {boolean}
- */
 export default function isValidPassword(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
 
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
+  if (password.length !== 10) return false;
+  if (!/^[0-9a-zA-Z]+$/.test(password)) return false;
+  
+  if (!(/[0-9]/.test(password) && /[a-zA-Z]/.test(password))) return false;
+  
+  if (password === password.toLowerCase() || password === password.toUpperCase()) return false;
+  if (forbiddenPasswords.includes(password)) return false;
+
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
+
+  const digitSequences = ['0123456789', '9876543210'];
+  for (let sequence of digitSequences) {
+    for (let start = 0; start <= sequence.length - 3; start++) {
+      const subSequence = sequence.substr(start, 3);
+      if (password.includes(subSequence)) return false;
+    }
+  }
+  
   return true;
 }
