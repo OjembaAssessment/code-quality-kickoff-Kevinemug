00535dfd484a98b5e49cfa1fccb2df076c8e3626
Mugisha Kevine <Kevinemug>
2023-10-09 15:10:17 +0200

    improvement 3 all test passed

--- src/improvement-3.js
+++ src/improvement-3.js
@@ -1,16 +1,4 @@
-/**
- *  TRY TO IMPROVE THE FOLLOWING FUNCTION.
- *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
- *  PLEASE DO NOT REMOVE `export default function`
- */
-
-// FUNCTION 3
 export default function findLargestNumber(numbers) {
-  let largest = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-      largest = numbers[i];
-    }
-  }
-  return largest;
+  if (!Array.isArray(numbers) || numbers.length === 0) return null;
+  return Math.max(...numbers);
 }
