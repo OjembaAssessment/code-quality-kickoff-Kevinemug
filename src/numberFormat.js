4e41456cbabfe61f7b4f682325576173f650cea2
Mugisha Kevine <Kevinemug>
2023-10-20 09:33:08 +0200

    feat: numberFormat

---
+++ src/numberFormat.js
@@ -0,0 +1,13 @@
+// Format any integer provided into a string with "," (commas) 
+//in the correct places.
+
+// Example:
+
+// For n = 100000 the function should return '100,000';
+// For n = 5678545 the function should return '5,678,545';
+// for n = -420902 the function should return '-420,902'.
+
+const numberFormat =  (number) => {
+    return number.toLocaleString();
+  };
+  console.log(numberFormat(2000000000))
--- src/romanEncoder.js
+++ src/romanEncoder.js
@@ -15,7 +15,7 @@ function solution(number) {
         decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
         roman    = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   
-    decimals.map(function (value, index) {
+    decimals.map( (value, index)=> {
       while (number >= value) {
         result += roman[index];
         number -= value;
